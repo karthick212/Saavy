98f280435c730277aa29b3952580c6d80b9ed7bf
karthick212 <karthick212>
2020-03-09 11:26:04 +0530

    secd

---
+++ precache-manifest.6108cb05e83bd3cbcb8772c43d317c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2478dd2d2f1693c0136887fb9a4edb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55fb6798ab1c96934417",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7aa96b81d0413283388e",
+    "url": "/static/js/2.d5b7e395.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d5b7e395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fb6798ab1c96934417",
+    "url": "/static/js/main.29bf15cb.chunk.js"
+  },
+  {
+    "revision": "530f40fe70386cd3a64e",
+    "url": "/static/js/runtime-main.681b83a1.js"
+  }
+]);
